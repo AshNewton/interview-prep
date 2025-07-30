b50b1af42055a25a421f5efb9741d1b22b4bff84
Ashley Newton <ash-newton@outlook.com>
2025-07-30 12:29:59 -0400

    count frequency

--- src/main/java/org/example/util/StringProblems.java
+++ src/main/java/org/example/util/StringProblems.java
@@ -1,5 +1,10 @@
 package org.example.util;
 
+import io.micrometer.common.util.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class StringProblems {
 
     public static String reverseString(String s) {
@@ -8,5 +13,22 @@ public class StringProblems {
         }
 
         return new StringBuffer(s).reverse().toString();
+    }
+
+    public static Map<Character, Integer> countFrequency(String s) {
+        Map<Character, Integer> histogram = new HashMap<>();
+
+        if (StringUtils.isEmpty(s)) {
+            return histogram;
+        }
+
+        for (int i=0; i<s.length(); i++) {
+            Character c = s.charAt(i);
+            Integer newCount = histogram.getOrDefault(c, 0);
+
+            histogram.put(c, newCount+1);
+        }
+
+        return histogram;
     }
 }
--- src/test/java/util/StringProblemsTest.java
+++ src/test/java/util/StringProblemsTest.java
@@ -3,6 +3,9 @@ package util;
 import org.example.util.StringProblems;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
@@ -27,5 +30,41 @@ public class StringProblemsTest {
     @Test
     public void testReverseSmall() {
         assertEquals("1", StringProblems.reverseString("1"));
+    }
+
+    @Test
+    public void testCountDuplicates() {
+        String s = "hello";
+        Map<Character, Integer> expected = Map.of(
+                'h', 1,
+                'e', 1,
+                'l', 2,
+                'o', 1);
+
+        testCountDuplicatesHelper(s, expected);
+    }
+
+    private void testCountDuplicatesHelper(String s, Map<Character, Integer> expected) {
+        Map<Character, Integer> actual = StringProblems.countFrequency(s);
+
+        assertEquals(expected.size(), actual.size());
+        actual.keySet().forEach(key -> {
+            assertEquals(expected.get(key), actual.get(key));
+        });
+    }
+
+    @Test
+    public void testCountDuplicatesEmpty() {
+        assertEquals(Collections.emptyMap(), StringProblems.countFrequency(null));
+        assertEquals(Collections.emptyMap(), StringProblems.countFrequency(""));
+    }
+
+    @Test
+    public void testCountDuplicatesAllSame() {
+        String s = "     ";
+        Map<Character, Integer> expected = Map.of(
+                ' ', s.length());
+
+        testCountDuplicatesHelper(s, expected);
     }
 }
